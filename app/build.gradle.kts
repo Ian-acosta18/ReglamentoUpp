488740e05a2853caa12d4aae6d590359650daf1c
Ian Acosta <ian.acostaoz@uppuebla.edu.mx>
2025-11-15 20:09:08 -0600

    version 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,9 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation("androidx.gridlayout:gridlayout:1.0.0")
+    // ... otras dependencias
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.cardview:cardview:1.0.0")
 }
